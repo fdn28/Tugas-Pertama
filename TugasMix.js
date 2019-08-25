8db7a564ec55718963f8649d22b31f2feb99318f
Mawar <fardanaja565@gmail.com>
2019-08-25 12:13:57 +0700

    ....

---
+++ TugasMix.js
@@ -0,0 +1,26 @@
+//conditional
+if( 5 != 5){
+    console.log("[IF:ELSE] 5 tidak sama dengan 5");
+}else{
+    console.log("[IF:ELSE] 5 sama dengan 5");
+}
+
+// if else if
+if ( 1 == 10){
+    console.log('[IF:Else IF] 10 tidak sama dengan 1');
+}else if ( 1 >= 0){
+    console.log('[IF:ELSE IF] tapi 1 lebih besar dari 0');
+}
+//looping
+var names = ["steve", "agus", "Pega", "Asna"];
+var i = 2;
+while(i < names.length) {
+    console.log(names[i]);
+    i++;
+}
+var buah = "apel" "anggur"  "nanas"  "pepaya";
+var i = 2;
+while(i < buah.length){
+    console.log(buah[i]);
+    i++;
+}
